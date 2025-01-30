6d0f62c947534b789f70362a59f258caed845ca5
Dimitri basseguy <dimitri.basseguy@gmail.com>
2025-01-30 21:33:21 +0100

    MVP

---
+++ showerScript.js
@@ -0,0 +1,33 @@
+function calculerCout() {
+  const prixKWh = parseFloat(document.getElementById('prixKWh').value);
+  const litreMinute = parseFloat(document.getElementById('litreMinute').value);
+  const prixM3 = parseFloat(document.getElementById('prixM3').value);
+  const nbDouchesInput = document.getElementById('nbDouches').value;
+
+  if (isNaN(prixKWh) || isNaN(litreMinute) || isNaN(prixM3) || nbDouchesInput.trim() === "") {
+    alert("Veuillez remplir tous les champs correctement.");
+    return;
+  }
+
+  const nbDouches = nbDouchesInput.split('+').map(Number).reduce((a, b) => a + b, 0);
+
+  const prixLitreEau = prixM3 / 1000; // Prix d'1 litre d'eau
+  const coutEauMinute = litreMinute * prixLitreEau;
+  const coutGazMinute = 0.04652 * prixKWh; // Estimation de 0,04652 kWh consommé par minute
+  const resultats = document.getElementById('resultat');
+  resultats.innerHTML = '';
+
+  [1, 5, 10, 15, 20].forEach(minute => {
+    const coutGazTotal = (coutGazMinute * minute).toFixed(2);
+    const coutEauTotal = (coutEauMinute * minute).toFixed(2);
+    const coutAnnuel = (nbDouches * 52 * (parseFloat(coutGazTotal) + parseFloat(coutEauTotal))).toFixed(2);
+
+    resultats.innerHTML += `
+          <tr>
+              <td class="border p-2 text-center">${minute}</td>
+              <td class="border p-2 text-center">${coutGazTotal} €</td>
+              <td class="border p-2 text-center">${coutEauTotal} €</td>
+              <td class="border p-2 text-center">${coutAnnuel} €</td>
+          </tr>`;
+  });
+}
